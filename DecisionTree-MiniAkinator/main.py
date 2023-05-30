ed0598c24615708b9c040e9c82870d18f84786d0
JoseMVasconcelos <josevictorvasc@gmail.com>
2023-05-30 20:36:45 -0300

    Akinator

---
+++ DecisionTree-MiniAkinator/main.py
@@ -0,0 +1,4 @@
+from controllers.game_controller import GameController
+
+if __name__ == "__main__":
+    GameController().start()
---
+++ DecisionTree-MiniAkinator/views/tree_view.py
@@ -0,0 +1,3 @@
+class TreeView:
+    def __init__(self, tree_controller):
+        self.__tree_controller = tree_controller
